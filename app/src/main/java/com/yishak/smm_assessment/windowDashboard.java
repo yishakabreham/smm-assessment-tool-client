8df32354bc23a19500d7dbd31f5414eeeba4aad2
yishakabreham <yoniyishakab@gmail.com>
2023-06-24 15:16:28 +0300

    initial commit

---
+++ app/src/main/java/com/yishak/smm_assessment/windowDashboard.java
@@ -0,0 +1,14 @@
+package com.yishak.smm_assessment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class windowDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_window_dashboard);
+    }
+}
---
+++ app/src/main/java/com/yishak/smm_assessment/windowSubPractices.java
@@ -0,0 +1,14 @@
+package com.yishak.smm_assessment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class windowSubPractices extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_window_sub_practices);
+    }
+}
